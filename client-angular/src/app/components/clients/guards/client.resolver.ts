ed4adaf9ac8ce7c536fb7c077342e83269fd9d35
Fanaina Alves <fanaiina.allves@gmail.com>
2024-03-28 15:24:58 -0300

    correção da implementação do client.resolver

--- client-angular/src/app/components/clients/guards/client.resolver.ts
+++ client-angular/src/app/components/clients/guards/client.resolver.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, MaybeAsync, ResolveFn, RouterStateSnapshot } from '@angular/router';
+import { ActivatedRouteSnapshot, Resolve, RouterStateSnapshot } from '@angular/router';
 import { ClientsService } from '../services/clients.service';
 import { Observable, of } from 'rxjs';
 import { Client } from '../models/client';
@@ -7,11 +7,11 @@ import { Client } from '../models/client';
 @Injectable({
   providedIn: 'root'
 })
-export class ClientResolver implements ResolveFn<Client> {
+export class ClientResolver implements Resolve<Client> {
 
   constructor(private service: ClientsService){}
 
-  resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): MaybeAsync<Client>{
+  resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<Client>{
     if(route.params && route.params['id']){
       return this.service.findById(route.params['id']);
     }
